eb0f7d89f11416ee34d3cdde8f760de8159bcf9f
thecomet <alex.murray@gmx.ch>
2013-10-25 16:25:36 +0200

    Base class TileManipulator written

--- chocobun-core/core/TileManipulator.cpp
+++ chocobun-core/core/TileManipulator.cpp
@@ -23,17 +23,89 @@
 // include files
 
 #include <core/TileManipulator.hpp>
+#include <core/Exception.hpp>
 
 namespace Chocobun {
 
 // --------------------------------------------------------------
-TileManipulator::TileManipulator( void )
+template <class COORD>
+TileManipulator<COORD>::TileManipulator( void )
 {
 }
 
 // --------------------------------------------------------------
-TileManipulator::~TileManipulator( void )
+template <class COORD>
+TileManipulator<COORD>::~TileManipulator( void )
 {
+}
+
+// --------------------------------------------------------------
+template <class COORD>
+void TileManipulator<COORD>::moveTile( const COORD& oldPos, const COORD& newPos ) const
+{
+    if( this->_moveTile( oldPos, newPos ) )
+        this->dispatchMoveTile( oldPos, newPos );
+}
+
+// --------------------------------------------------------------
+template <class COORD>
+void TileManipulator<COORD>::setTile( const COORD& pos, const char& tile ) const
+{
+    if( this->_setTile( pos, tile ) )
+        this->dispatchSetTile( pos, tile );
+}
+
+// --------------------------------------------------------------
+template <class COORD>
+void TileManipulator<COORD>::addListener( const TileManipulator::Listener* listener )
+{
+    for( typename std::vector<TileManipulator::Listener*>::iterator it = m_Listeners.begin(); it != m_Listeners.end(); ++it )
+        if( *it == listener )
+            throw Exception( "[TileManipulator::addListener] Error: Listener already registered" );
+    m_Listeners.push_back( listener );
+}
+
+// --------------------------------------------------------------
+template <class COORD>
+void TileManipulator<COORD>::removeListener( const TileManipulator::Listener* listener )
+{
+    for( typename std::vector<TileManipulator::Listener*>::iterator it = m_Listeners.begin(); it != m_Listeners.end(); ++it )
+        if( *it == listener )
+        {
+            m_Listeners.erase( it );
+            return;
+        }
+    throw Exception( "[TileManipulator::removeListener] Error: Listener not registered" );
+}
+
+// --------------------------------------------------------------
+template <class COORD>
+bool TileManipulator<COORD>::_moveTile( const COORD& oldPos, const COORD& newPos )
+{
+    return false; // disable listener dispatching
+}
+
+// --------------------------------------------------------------
+template <class COORD>
+bool TileManipulator<COORD>::_setTile( const COORD& pos, const char& tile )
+{
+    return false; // disable listener dispatching
+}
+
+// --------------------------------------------------------------
+template <class COORD>
+void TileManipulator<COORD>::dispatchMoveTile( const COORD& oldPos, const COORD& newPos ) const
+{
+    for( typename std::vector<TileManipulator::Listener*>::const_iterator it = m_Listeners.begin(); it != m_Listeners.end(); ++it )
+        (*it)->onMoveTile( oldPos, newPos );
+}
+
+// --------------------------------------------------------------
+template <class COORD>
+void TileManipulator<COORD>::dispatchSetTile( const COORD& pos, const char& tile ) const
+{
+    for( typename std::vector<TileManipulator::Listener*>::const_iterator it = m_Listeners.begin(); it != m_Listeners.end(); ++it )
+        (*it)->onSetTile( pos, tile );
 }
 
 } // namespace Chocobun
--- chocobun-core/core/TileManipulator.hpp
+++ chocobun-core/core/TileManipulator.hpp
@@ -22,11 +22,28 @@
 // --------------------------------------------------------------
 // include files
 
+#include <vector>
+
 namespace Chocobun {
 
+// --------------------------------------------------------------
+// forward declarations
+
+template <class COORD>
 class TileManipulator
 {
 public:
+
+    /*!
+     * @brief Listener interface class for tile manipulation notifications
+     */
+    class Listener
+    {
+    protected:
+
+        void onMoveTile( const COORD& oldPos, const COORD& newPos );
+        void onSetTile( const COORD& pos, const char& tile );
+    };
 
     /*!
      * @brief Default constructor
@@ -39,9 +56,66 @@ public:
     virtual ~TileManipulator( void );
 
     /*!
-     *
+     * @brief Moves a tile from one position to another
      */
+    void moveTile( const COORD& oldPos, const COORD& newPos ) const;
 
+    /*!
+     * @brief Changes the tile from one type to another
+     */
+    void setTile( const COORD& pos, const char& tile ) const;
+
+    /*!
+     * @brief Add a listener to be informed about tile manipulations
+     */
+    void addListener( const Listener* listener );
+
+    /*!
+     * @brief Remove a registered listener
+     */
+    void removeListener( const Listener* listener);
+
+protected:
+
+    /*!
+     * @brief Abstract method for moving a tile from one position to another
+     * If the method is not implemented by its derived class, the listeners
+     * will not be informed about any tile moves, even if @a moveTile is
+     * called. In order to enable listener notifications, the derived class
+     * must return **true**. Returning **false** will disable notifications
+     * for tile moves.
+     * @param oldPos The coordinates of the tile to move
+     * @param newPos The destination coordinates of the selected tile
+     * @return True to notify listeners, false to not notify listeners
+     */
+    virtual bool _moveTile( const COORD& oldPos, const COORD& newPos );
+
+    /*!
+     * @brief Abstract method for changing a tile from one type to another
+     * If the method is not implemented by its derived class, the listeners
+     * will not be informed about any tile changes, even if @a setTile is
+     * called. In order to enable listener notifications, the derived class
+     * must return **true**. Returning **false** will disable notifications
+     * for tile changes.
+     * @param pos The coordinates of the tile to set
+     * @param tile The type of tile to change it to
+     * @return True to notify listeners, false to not notify listeners
+     */
+    virtual bool _setTile( const COORD& pos, const char& tile );
+
+private:
+
+    /*!
+     * @brief Dispatches a tile move event to all registered listeners
+     */
+    void dispatchMoveTile( const COORD& oldPos, const COORD& newPos ) const;
+
+    /*!
+     * @brief Dispatches a tile set event to all registered listeners
+     */
+    void dispatchSetTile( const COORD& pos, const char& tile ) const;
+
+    std::vector<Listener*> m_Listeners;
 
 };
 
